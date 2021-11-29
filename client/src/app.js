c8b57fe12e12932034b15344f1e36cfbdb022b2b
Deo Kumar <40450564+Deokumarjnu@users.noreply.github.com>
2021-11-29 19:43:59 +0530

    Added docker file

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,4 @@
-import react from "react";
+import React from "react";
 
 const App = () => {
     return <div><h1>App</h1></div>
--- server/index.js
+++ server/index.js
@@ -12,7 +12,6 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
-
 const CONNECTION_URL = "mongodb+srv://root:root123@cluster0.krr0r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT|| 5000;
 
